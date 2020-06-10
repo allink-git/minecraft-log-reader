b4702e58b3f4e54a0fe7e673ba05e2efc4113db9
arclicious <44676012+arclicious@users.noreply.github.com>
2020-06-10 20:45:44 +0100

    Add code

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Minecraft_Log_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputbtn = new System.Windows.Forms.Button();
+            this.outputlbl = new System.Windows.Forms.Label();
+            this.inputlbl = new System.Windows.Forms.Label();
+            this.inputbtn = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.FolderBrowserDialog();
+            this.output = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // outputbtn
+            // 
+            this.outputbtn.Location = new System.Drawing.Point(12, 52);
+            this.outputbtn.Name = "outputbtn";
+            this.outputbtn.Size = new System.Drawing.Size(82, 37);
+            this.outputbtn.TabIndex = 0;
+            this.outputbtn.Text = "Output File";
+            this.outputbtn.UseVisualStyleBackColor = true;
+            this.outputbtn.Click += new System.EventHandler(this.outputbtn_Click);
+            // 
+            // outputlbl
+            // 
+            this.outputlbl.AutoSize = true;
+            this.outputlbl.Location = new System.Drawing.Point(96, 63);
+            this.outputlbl.Name = "outputlbl";
+            this.outputlbl.Size = new System.Drawing.Size(61, 13);
+            this.outputlbl.TabIndex = 1;
+            this.outputlbl.Text = "Output File:";
+            // 
+            // inputlbl
+            // 
+            this.inputlbl.AutoSize = true;
+            this.inputlbl.Location = new System.Drawing.Point(95, 25);
+            this.inputlbl.Name = "inputlbl";
+            this.inputlbl.Size = new System.Drawing.Size(79, 13);
+            this.inputlbl.TabIndex = 3;
+            this.inputlbl.Text = "Input Directory:";
+            // 
+            // inputbtn
+            // 
+            this.inputbtn.Location = new System.Drawing.Point(12, 12);
+            this.inputbtn.Name = "inputbtn";
+            this.inputbtn.Size = new System.Drawing.Size(82, 37);
+            this.inputbtn.TabIndex = 2;
+            this.inputbtn.Text = "Input Directory";
+            this.inputbtn.UseVisualStyleBackColor = true;
+            this.inputbtn.Click += new System.EventHandler(this.inputbtn_Click);
+            // 
+            // output
+            // 
+            this.output.DefaultExt = "*.txt";
+            this.output.FileName = "C:\\";
+            this.output.FileOk += new System.ComponentModel.CancelEventHandler(this.output_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 104);
+            this.Controls.Add(this.inputlbl);
+            this.Controls.Add(this.inputbtn);
+            this.Controls.Add(this.outputlbl);
+            this.Controls.Add(this.outputbtn);
+            this.Name = "Form1";
+            this.Text = "Minecraft Log Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button outputbtn;
+        private System.Windows.Forms.Label outputlbl;
+        private System.Windows.Forms.Label inputlbl;
+        private System.Windows.Forms.Button inputbtn;
+        private System.Windows.Forms.FolderBrowserDialog input;
+        private System.Windows.Forms.SaveFileDialog output;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace Minecraft_Log_Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        static byte[] Decompress(byte[] gzip)
+        {
+            // Create a GZIP stream with decompression mode.
+            // ... Then create a buffer and write into while reading from the GZIP stream.
+            using (GZipStream stream = new GZipStream(new MemoryStream(gzip),
+                CompressionMode.Decompress))
+            {
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    return memory.ToArray();
+                }
+            }
+        }
+
+        private void logs()
+        {
+            List<String> lines = new List<String>();
+            if(input.SelectedPath != "" && output.FileName != "C:\\")
+            {
+                if (System.IO.Directory.Exists(input.SelectedPath))
+                {
+                    foreach(String path in System.IO.Directory.GetFiles(input.SelectedPath))
+                    {
+                        if (path.ToLower().EndsWith(".log.gz"))
+                        {
+                            byte[] file = File.ReadAllBytes(@path);
+                            byte[] decompressed = Decompress(file);
+                            lines.Add(System.Text.Encoding.Default.GetString(decompressed));
+                        }
+                    }
+                }
+            }
+            if(lines.Count > 0)
+            {
+                File.WriteAllLines(this.output.FileName, lines.ToArray());
+            } else
+            {
+                //MessageBox.Show("No compressed logs were found. Did you select the correct directory?", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void inputbtn_Click(object sender, EventArgs e)
+        {
+
+            DialogResult input = this.input.ShowDialog();
+            if(input == DialogResult.OK && !string.IsNullOrWhiteSpace(this.input.SelectedPath))
+            {
+                this.inputlbl.Text = "Input Directory: " + this.input.SelectedPath;
+                logs();
+            } else
+            {
+                MessageBox.Show("Folder was corrupted or invalid. Did you select a working folder?",this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void outputbtn_Click(object sender, EventArgs e)
+        {
+            output.ShowDialog();
+        }
+
+        private void output_FileOk(object sender, CancelEventArgs e)
+        {
+            outputlbl.Text = "Output File: " + output.FileName;
+            logs();
+        }
+    }
+}
